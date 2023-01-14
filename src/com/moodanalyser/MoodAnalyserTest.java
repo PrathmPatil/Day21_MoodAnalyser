63ebb3de1daf635dde0910e3e46b0e6219c0fa53
PrathmPatil <prathmpatil2818@gmail.com>
2023-01-14 20:35:10 +0530

    Check user happy or not

--- src/com/moodanalyser/MoodAnalyserTest.java
+++ src/com/moodanalyser/MoodAnalyserTest.java
@@ -2,10 +2,7 @@ package com.moodanalyser;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 
 class MoodAnalyserTest {
@@ -13,9 +10,9 @@ class MoodAnalyserTest {
 	@Test
 	void  testanalyserMood() {
 		MoodAnalyser moodanalyser=new MoodAnalyser();
-		String mood="sad";
+		String mood="happy";
 		String actual=moodanalyser.analyserMood(mood);
-		assertEquals("sad", actual);
+		assertEquals("happy", actual);
 	}
 
 }
